d9d833cfa1f3e6699c9b177e9cdececb64e020ad
ntsmitty <nicktsmith7@gmail.com>
2018-10-09 19:19:13 -0700

    added routes for google auth

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+const authRoutes = require('./routes/auth-routes.js');
+const passportSetup = require('./passport-config.js')
 const PORT = 3333;
+
+
+app.use('/auth', authRoutes)
+
+app.use(express.static(path.join(__dirname, '../build')))
+
+app.get('/', (req, res) => {
+    res.status(200)
+    res.sendFile(path.join(__dirname, '../build/index.html'));  
+}); 
 
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
