05849a532fcc119575e0baf636119e4321e4521e
Colin Roemer <colin.roemer@gmail.com>
2018-10-10 13:36:35 -0700

    Adding some game logic - ready for implementing socket connection and thinking that through

--- build/game.js
+++ build/game.js
@@ -1,29 +1,37 @@
 //Game logic here!  should be wrapped and only triggered once lobby is full and ready to play
 let user;
+let gameStatus = 'waiting';  // Temporary variable - gameStatus should be coming from socket constantly
 
 class Player {
   constructor() {
-    this.type = 'Shark';
-    this.id = null;
-    this.status = null;
+    this.type = 'minnow';
+    this.id = null; // Should represent socket connection ID so socket can identify players
+    this.status = 'alive'; // Values should be alive, dead, safe?
     this.pos = createVector(width/2, height/2);
-    
   }
 
-  move(speed) {
-    const vel = createVector(mouseX - width / 2, mouseY - height / 2);
-    vel.setMag(speed);
-    this.pos.add(vel);
+  move() {
+    if (this.status === 'alive') {
+      const vel = createVector(mouseX - width / 2, mouseY - height / 2);
+      vel.setMag(this.speed);
+      this.pos.add(vel);
+    }
   }
 
+  // Sets bounds player can move - this might need to move out of player and into something related to the board
   constrain() {
     this.pos.x = constrain(user.pos.x, 0 - this.r, 6000 - this.r);
     this.pos.y = constrain(user.pos.y, 0 + this.r, 6000 - this.r);
   }
 
   show() {
-    // fill(255);
-    ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2)
+    if (this.type === 'shark') {
+      fill('red');
+      ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2)
+    } else { 
+      fill('blue');
+      ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2)
+    }
   }
 }
 
@@ -41,11 +49,12 @@ class Minnow extends Player {
   constructor() {
     super()
     this.r = 20
-    this.speed = 100
+    this.speed = 20
   }
 
 }
 
+// Was thinking people could be Watchers if the game is full - view would be of entire board - Stretch feature
 class Watcher extends Player {
   constructor() {
     super()
@@ -55,20 +64,32 @@ class Watcher extends Player {
 //create board
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  user = new Minnow()
+  user = new Minnow();
+
+  // Temp function to wait for game to start
+  setTimeout(() => {
+    gameStatus = 'started';
+  }, 5000)
 }
 
 function draw() {
-  background('blue');
+  background('#ececec');
   translate((width / 2) - user.pos.x, (height / 2) - user.pos.y)
+  
   user.show();
-  user.move(user.speed);
-  user.constrain();
+
+  if (gameStatus === 'started') {
+    user.move();
+  }
+  
+  // Visual Game Boarder
+  fill('rgba(0,255,55, 0.25)');
   rect(0, 0, 6000, 6000);
-  fill('rgba(0,255,0, 0.25)');
 
   if (user.pos.x < 0 - user.r * 2) {
-    noLoop()
+    noLoop(); // SAFE ZONE!!!! NO more moving
+  } else {
+    user.constrain(); // Not in safe zone - keep setting constraints 
   }
 
 }
