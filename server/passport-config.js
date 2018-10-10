79df9d560033ba2c935fd6bbba126abca1a4df43
ntsmitty <nicktsmith7@gmail.com>
2018-10-09 19:20:30 -0700

    passport config for google auth

---
+++ server/passport-config.js
@@ -0,0 +1,15 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./keys.js')
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.google.clientID, 
+    clientSecret: keys.google.clientSecret,
+    callbackURL: '/auth/google/redirect',
+  },
+  function(accessToken, refreshToken, profile, done ) {  
+    console.log(profile)
+    return done(err, user) 
+  })
+);
