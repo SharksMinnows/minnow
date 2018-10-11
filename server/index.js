90c8c4c20029469f8c6196c4faa823438a2c454c
Joshua <jwrightbluj@gmail.com>
2018-10-10 23:51:56 -0700

    Minnows send data to server, server sends data back to minnows

--- server/index.js
+++ server/index.js
@@ -1,28 +1,43 @@
 require('dotenv/config')
-const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./graphql-schema');
+// const schema = require('./graphql-schema');
 const path = require('path');
+const PORT = process.env.PORT || 3333;
+
+const express = require('express');
+const socket = require('socket.io')
+
+let currentGlobalIncrementingID = 0;
+
+let allPlayers = {};
+
 const app = express();
-const PORT = process.env.PORT || 5000;
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
-
-const connections = [];
-
-server.listen(3000);
-
-app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`)
+const server = app.listen(3333, () => {
+    console.log(`Listening on 3333`)
 });
 
-app.use(express.static(path.join(__dirname, './public')))
+const io = socket(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '../build/bundle.js');
+io.on('connection', (socket) => {
+    console.log('Somebody connected to the socket!', socket.id);
+    socket.emit('setID', ++currentGlobalIncrementingID);
+    socket.on('update', data => {
+        console.clear()
+        allPlayers[data.id] = data;
+        console.log(allPlayers);
+
+        if(data.id === null){
+            socket.emit('setID', ++currentGlobalIncrementingID);
+        }
+        socket.emit('update', allPlayers);
+    })
 })
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: false //Set to true to view GraphiQl in browser at /graphql
-}));
+app.use(express.static(path.join(__dirname, '../build')))
+
+
+
+// app.use('/graphql', graphqlHTTP({
+//     schema,
+//     graphiql: false //Set to true to view GraphiQl in browser at /graphql
+// }));
